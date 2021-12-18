0c22b79e8e77e113c2c35e3e92436ef5505d375a
madis <madis.pihlasalu@gmail.com>
2021-12-18 14:05:48 +0200

    task2 update

--- task2.js
+++ task2.js
@@ -1,5 +1,23 @@
 
 require('dns').lookup(require('os').hostname(), function (err, add, fam) {
     console.log(add);
+    
   })
+
+  // --- Pikem versioon sama tulemus----
+
+  function showIpAddress() {
+    var interfaces = require('os').networkInterfaces();
+    for (var devName in interfaces) {
+      var iface = interfaces[devName];
+  
+      for (var i = 0; i < iface.length; i++) {
+        var alias = iface[i];
+        if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+          return alias.address;
+      }
+    }
+    return '0.0.0.0';
+  }
+  console.log(showIpAddress())
 
